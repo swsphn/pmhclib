b3340c592dcd2f42f32bbe790f0fc234f968be55
David Wales <david.wales@swsphn.com.au>
2023-11-30 16:35:16 +1100

    Add top level imports

---
+++ src/pmhclib/__init__.py
@@ -0,0 +1,6 @@
+# Import classes to be available at the top level.
+# Allows importing with:
+#     from pmhclib import PMHC
+# rather than
+#     from pmhclib.pmhc import PMHC
+from .pmhc import PMHC
