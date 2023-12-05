1111d4ce4f8036956c1d06033eeeccea9aa97a0f
David Wales <david.wales@swsphn.com.au>
2023-12-06 09:42:33 +1100

    Add objects to package top level

--- src/pmhclib/__init__.py
+++ src/pmhclib/__init__.py
@@ -1,6 +1,10 @@
+# read version from installed package
+from importlib.metadata import version
+__version__ = version("pmhclib")
+
 # Import classes to be available at the top level.
 # Allows importing with:
 #     from pmhclib import PMHC
 # rather than
 #     from pmhclib.pmhc import PMHC
-from .pmhc import PMHC
+from .pmhc import PMHC, PMHCSpecification
