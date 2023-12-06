f8ecc0ccaffe419cd9d1e7c184c604af2d7849cb
David Wales <david.wales@swsphn.com.au>
2023-12-06 15:05:16 +1100

    Blacken conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,29 +6,32 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'pmhclib'
-copyright = '2023, David Wales, Jonathan Stucken, South Western Sydney Primary Health Network'
-author = 'David Wales, Jonathan Stucken, South Western Sydney Primary Health Network'
-release = '0.2.0'
+project = "pmhclib"
+copyright = (
+    "2023, David Wales, Jonathan Stucken, South Western Sydney Primary Health Network"
+)
+author = "David Wales, Jonathan Stucken, South Western Sydney Primary Health Network"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser', 'sphinx.ext.autodoc', 'autoapi.extension']
+extensions = ["myst_parser", "sphinx.ext.autodoc", "autoapi.extension"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 
 # -- Options for autoapi
-autoapi_dirs = ['../src/pmhclib']
+autoapi_dirs = ["../src/pmhclib"]
 # autoapi_keep_files = True
+
 
 # Only document top level package (skip sub-modules)
 def skip_submodules(app, what, name, obj, skip, options):
@@ -42,4 +45,4 @@ def setup(sphinx):
 
 
 # -- Render type annotations in function description
-autodoc_typehints = 'description'
+autodoc_typehints = "description"
